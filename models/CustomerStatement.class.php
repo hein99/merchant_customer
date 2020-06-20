32de0627fc049b33817ee12bd5f8fc63871ed136
SweSwe-Nyein <sweswe4720@gmail.com>
2020-06-20 15:25:16 +0630

    done bill history

--- models/CustomerStatement.class.php
+++ models/CustomerStatement.class.php
@@ -10,6 +10,26 @@ class CustomerStatement extends DataObject
     'created_date' => ''
   );
 
+  public static function getCustomerStatement($customer_id)
+  {
+    $conn = parent::connect();
+    $sql = 'SELECT * FROM '.TBL_CUSTOMER_STATEMENT.' WHERE customer_id = :customer_id ORDER BY created_date DESC';
+    try {
+      $st = $conn->prepare($sql);
+      $st->bindValue(':customer_id', $customer_id, PDO::PARAM_INT);
+      $st->execute();
+      $statements = array();
+      foreach($st->fetchAll() as $row)
+      {
+        $statements[] =  new CustomerStatement($row);
+      }
+      parent::disconnect($conn);
+      return $statements;
+    } catch (PDOException $e) {
+      parent::disconnect($conn);
+      die('Query failed: ' . $e->getMessage());
+    }
+  }
 
 }
  ?>
