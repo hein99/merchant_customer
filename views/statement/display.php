8fb0fc714721b9abf04a4e0cb381e3006a514b21
khinyu2000 <khinyu2000@gmail.com>
2020-06-25 08:00:22 -0400

    bill history

--- views/statement/display.php
+++ views/statement/display.php
@@ -5,37 +5,34 @@ displayOtherNavigation('statement');
 $customer_statements = CustomerStatement::getCustomerStatement($_SESSION['merchant_customer_account']->getValueEncoded('id'));
 $customer_acc = UsersAccount::getCustomerAccountById($_SESSION['merchant_customer_account']->getValueEncoded('id'));
  ?>
- <section>
-   <div class="ssn_loader">
-     <div class="triple-spinner"></div>
+ <section class="ky-bill-history" >
+   <div class="ssn_loader" >
+     <div class="triple-spinner" ></div>
    </div>
-   <div class="">
-     <h1>Totol Balance</h1>
-     <span><?php echo number_format($customer_acc->getValueEncoded('balance'), 2) ?></span>&nbsp;Ks
+   <div class="ky-total-balance-container" >
+     <h1 class="ky-balance-header" >Total Balance</h1>
+     <span class="ky-total-balance" ><?php echo number_format($customer_acc->getValueEncoded('balance'), 2) ?>&nbsp;&nbsp;MMK</span>
    </div>
-   <div class="">
-     <h1>Transaction Histories</h1>
-     <table>
-       <thead>
-         <tr>
-           <th>Date</th>
-           <th>About</th>
-           <th>Amount</th>
-         </tr>
-       </thead>
-       <tbody>
-         <?php foreach ($customer_statements as $customer_statement): ?>
-           <tr>
-             <td><?php echo $customer_statement->getValueEncoded('created_date'); ?></td>
-             <td><?php echo $customer_statement->getValueEncoded('about'); ?></td>
-             <td class="<?php echo $customer_statement->getValue('amount_status') ? 'plus' : 'minus'?>"><span><?php echo $customer_statement->getValue('amount_status') ? '+' : '-'?></span><?php echo $customer_statement->getValueEncoded('amount'); ?>&nbsp;Ks</td>
-           </tr>
-         <?php endforeach; ?>
-       </tbody>
-       <tbody>
-
-       </tbody>
-     </table>
+   <div class="ky-transaction-history-container" >
+     <h1 class="ky-transaction-history-header" >Transaction History</h1>
+     <div class="ky-transaction-table-head">
+       <span>Date</span>
+       <span>About</span>
+       <span>Amount</span>
+     </div>
+     <div class="ky-transaction-table-body">
+       <table>
+         <tbody>
+           <?php foreach ($customer_statements as $customer_statement): ?>
+             <tr>
+               <td><?php echo $customer_statement->getValueEncoded('created_date'); ?></td>
+               <td><?php echo $customer_statement->getValueEncoded('about'); ?></td>
+               <td class="<?php echo $customer_statement->getValue('amount_status') ? 'plus' : 'minus'?>"><span><?php echo $customer_statement->getValue('amount_status') ? '+' : '-'?></span><?php echo $customer_statement->getValueEncoded('amount'); ?>&nbsp;Ks</td>
+             </tr>
+           <?php endforeach; ?>
+         </tbody>
+       </table>
+     </div>
    </div>
  </section>
 <?php
